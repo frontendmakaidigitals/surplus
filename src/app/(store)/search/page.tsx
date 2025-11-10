9add656df25b0532dddf8003c90b01702a216f86
faheem221 <faheemk793@gmail.com>
2025-11-10 16:40:14 +0530

    fixed type

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -4,40 +4,42 @@ import { publicUrl } from "@/env.mjs";
 import { Search } from "@/lib/api";
 import { ProductList } from "@/ui/products/product-list";
 import { ProductNotFound } from "@/ui/products/product-not-found";
-
+import { products } from "../../../../data";
 export const generateMetadata = async (props: {
-	searchParams: Promise<{
-		q?: string;
-	}>;
+  searchParams: Promise<{
+    q?: string;
+  }>;
 }): Promise<Metadata> => {
-	const searchParams = await props.searchParams;
-	const query = searchParams.q || "";
-	return {
-		title: `Search results for "${query}"`,
-		alternates: { canonical: `${publicUrl}/search` },
-	};
+  const searchParams = await props.searchParams;
+  const query = searchParams.q || "";
+  return {
+    title: `Search results for "${query}"`,
+    alternates: { canonical: `${publicUrl}/search` },
+  };
 };
 
 export default async function SearchPage(props: {
-	searchParams: Promise<{
-		q?: string;
-	}>;
+  searchParams: Promise<{
+    q?: string;
+  }>;
 }) {
-	const searchParams = await props.searchParams;
-	const query = searchParams.q;
+  const searchParams = await props.searchParams;
+  const query = searchParams.q;
 
-	if (!query) {
-		return redirect("/", RedirectType.replace);
-	}
+  if (!query) {
+    return redirect("/", RedirectType.replace);
+  }
 
-	const products = await Search.searchProducts(query);
-
-	return (
-		<main>
-			<h1 className="text-3xl font-bold leading-none tracking-tight text-foreground">
-				Search results for “{query}”
-			</h1>
-			
-		</main>
-	);
+  return (
+    <main>
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground">
+        Search results for “{query}”
+      </h1>
+      {products?.length ? (
+        <ProductList productData={products} />
+      ) : (
+        <ProductNotFound query={query} />
+      )}
+    </main>
+  );
 }
